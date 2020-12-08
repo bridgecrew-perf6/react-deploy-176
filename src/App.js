aea3b786d6d7560de64e4c5abf459ea7eda7664a
dudz <dudzpedra@outlook.com>
2020-12-08 16:32:08 -0300

    deploy to gh-pages

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './Img/muttley.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Empty React App ready for deploy!</h1>
+      <h2>Made by: DudzPedra</h2>
+      <img src={logo} alt="profile-img" />
+    </div>
+  );
+}
+
+export default App;
